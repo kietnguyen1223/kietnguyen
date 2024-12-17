c5d5abfce03c36b3461d08492864b1c2f5e207a9
kietnguyen1223 <nhak2310@gmail.com>
2024-12-17 09:17:05 +0700

    Add files via upload

---
+++ TypeData/src/basic/dev/MainApp1.java
@@ -0,0 +1,12 @@
+package basic.dev;
+
+public class MainApp1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        short n = 120;
+        byte b = (byte) n;
+        System.out.println(b);
+	}
+
+}
